59cce4c16106590cb43b20fc7c68158b0877a8c0
lajawanti <lajawanti.dhake@gmail.com>
2021-08-10 11:26:17 -0400

    header- search

--- airbnb-with-next.js/components/Header.js
+++ airbnb-with-next.js/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image';
+import { SearchIcon } from '@heroicons/react/solid';
 
 function Header() {
     return (
@@ -15,7 +16,18 @@ function Header() {
             </div>
 
             {/* Middle */}
-            <div></div>
+            <div
+                className= "flex items-center md:border-2 rounded-full py-2 md:shadow-sm"
+            >
+                <input 
+                    className= "flex-grow pl-5 bg-transparent outline-none"
+                    type= "text"
+                    placeholder= "Start your search"
+                />
+                <SearchIcon 
+                    className= "hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"
+                />
+            </div>
 
             {/* Right */}
             <div></div>
