4b18af38e77be3b2b8c809163a7fc71595a1936d
lajawanti <lajawanti.dhake@gmail.com>
2021-09-10 17:47:26 -0400

    explore nearby next after banner

--- airbnb-with-next.js/pages/index.js
+++ airbnb-with-next.js/pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div>
       <Head>
@@ -11,8 +11,29 @@ export default function Home() {
       </Head>
 
       {/* components */}
-      <Header /> 
+      <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore nearby</h2>
+          {/* Pull some data from a server - API endpoints here we are using static rendering using getStaticProps()*/}
+          {exploreData?.map((item) => (
+            <h1>{item.location}</h1>
+          ))}
+        </section>
+      </main>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
